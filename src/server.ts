0ce5284ebca5441cb86a0740b3c51893ea118217
resqiar <resqiageng@gmail.com>
2022-12-19 22:39:00 +0700

    use env variable for client origin

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import cors from "@fastify/cors";
 const server: FastifyInstance = fastify();
 
 server.register(cors, {
-  origin: ["http://localhost:5173"],
+  origin: [process.env.CLIENT_ORIGIN ?? ""],
 });
 
 server.get("/ping", async () => {
